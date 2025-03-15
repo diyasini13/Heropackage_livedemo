123873814af9801f4b9bcf04b23ae2ffeadb6f60
diyasini13 <diyasini@google.com>
2025-03-15 13:46:31 +0530

    Ui changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div className="h-4"></div>
+      <div className='text-center p-2 bg-gray-100'>
+        <p className='font-googlesans text-sm md:text-2lg'>Empower your business with Google Cloud Solutions Today</p>
+        <a href="https://github.com/GoogleCloudPlatform/click-to-deploy-solutions/tree/main" target="_blank" rel="noopener noreferrer" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">
+          Explore Hero Packages
+        </a>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,14 @@ function Header() {
       <div className='text-center p-2 bg-gray-100'>
         <p className='font-googlesans text-sm md:text-2xl'>Hero Packages: Ready-to-Deploy Google Cloud Solutions</p>
       </div>
-      <img className='' src={banner} alt="" />
+      <a href="https://cloud.google.com/?e=48754805" target="_blank" rel="noopener noreferrer">
+      <img className='' src={banner} alt="banner" sizes="" />
+      </a>
+      <div className="h-4"></div>
+      <div className="text-center p-2 bg-white-10"> {/* Added margin top for spacing */}
+            <p className="font-googlesans text-sm md:text-xl">Please fill the <a href="https://forms.gle/fC76Upee7H6d88j37" className="text-blue-600 underline" target="_blank">feedback form</a>.</p>
+        </div>
+      <div className="h-4"></div>
     </>
   )
 }
