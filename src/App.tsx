e410182c47a035605e1c02301d5b5ef4636208f5
diyasini13 <diyasini@google.com>
2025-03-16 14:55:35 +0530

    Google Outh set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,3 @@
-// import { useState } from "react";
-// import "./App.css";
-// import Header from "./components/Header";
-// import Body from "./components/Body";
-// import Footer from "./components/Footer";
-// import AuthPage from "./components/AuthPage";
-
-// function App() {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-//   return (
-//     <>
-//       {isAuthenticated ? (
-//         <>
-//           <Header />
-//           <Body />
-//           <Footer />
-//         </>
-//       ) : (
-//         <AuthPage onLoginSuccess={() => setIsAuthenticated(true)} />
-//       )}
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
 import { useState, useEffect } from 'react';
 import AuthPage from './components/AuthPage';
 import Body from './components/Body';
